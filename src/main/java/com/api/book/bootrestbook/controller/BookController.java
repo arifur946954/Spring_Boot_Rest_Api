c1d75744ef608d19af7a5c1e5efbdd312012235b
Arifur Rahman <arif.bd08060@gmail.com>
2023-08-16 23:42:41 +0600

    database connection

--- src/main/java/com/api/book/bootrestbook/controller/BookController.java
+++ src/main/java/com/api/book/bootrestbook/controller/BookController.java
@@ -1,21 +1,25 @@
 package com.api.book.bootrestbook.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.book.bootrestbook.entity.Book;
 import com.api.book.bootrestbook.service.BookService;
 
 @RestController
+// @RequestMapping("/api/books")
+
 public class BookController {
     @Autowired
     private BookService bookservices;
@@ -23,26 +27,45 @@ public class BookController {
     // get all book handeler
     @GetMapping("/books")
 
-    public List<Book> getBook() {
+    public ResponseEntity<List<Book>> getBook() {
         // Book book = new Book(123, "Atomic Habbit", "James Clear");
+        List<Book> list = this.bookservices.getAllBook();
 
-        return this.bookservices.getAllBook();
+        if (list.size() <= 0) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        } else {
+            return ResponseEntity.status(HttpStatus.CREATED).body(list);
+        }
     }
 
     // get single book handaler
     @GetMapping("/books/{id}")
-    public Book getBookB(@PathVariable("id") int id) {
-        return this.bookservices.getBookById(id);
+    public ResponseEntity<Book> getBookB(@PathVariable("id") int id) {
+        Book book = this.bookservices.getBookById(id);
+        if (book == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        } else {
+            return ResponseEntity.of(Optional.of(book));
+        }
 
     }
 
     // post book handaller
+
     @PostMapping("/books")
     public Book addBook(@RequestBody Book book) {
         Book b = this.bookservices.addBook(book);
         return b;
 
     }
+
+    // @PostMapping("/")
+    // public Book addBook(@RequestBody Book book) {
+    // Book b = this.bookservices.addBook(book);
+    // /// return new ResponseEntity<>(createUserDto,HttpStatus.CREATED);
+    // return b;
+
+    // }
 
     // delete boook maping
     @DeleteMapping("/books/{bookId}")
--- src/main/java/com/api/book/bootrestbook/service/BookService.java
+++ src/main/java/com/api/book/bootrestbook/service/BookService.java
@@ -1,55 +1,79 @@
 package com.api.book.bootrestbook.service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.api.book.bootrestbook.dao.BookRepository;
 import com.api.book.bootrestbook.entity.Book;
 
 @Component
 public class BookService {
-    private static List<Book> list = new ArrayList<>();
+    @Autowired
+    private BookRepository bookRepository;
+    // private static List<Book> list = new ArrayList<>();
 
-    static {
-        list.add(new Book(1, "javaScript the definite Guid", "David falangan"));
-        list.add(new Book(2, "Eat that frog", "Brain Tracy"));
-        list.add(new Book(3, "Rich Dad Poor Dad", "Robert T Kiosarr"));
-        list.add(new Book(4, "wings of fire", "APJ abdul kalam"));
-        list.add(new Book(5, "bela furabar age", "APJ abdul kalam"));
-    }
+    // static {
+    // list.add(new Book(1, "javaScript the definite Guid", "David falangan"));
+    // list.add(new Book(2, "Eat that frog", "Brain Tracy"));
+    // list.add(new Book(3, "Rich Dad Poor Dad", "Robert T Kiosarr"));
+    // list.add(new Book(4, "wings of fire", "APJ abdul kalam"));
+    // list.add(new Book(5, "bela furabar age", "APJ abdul kalam"));
+    // list.add(new Book(6, "post manr ", "kham abul kalam azad"));
+
+    // }
 
     public List<Book> getAllBook() {
+        List<Book> list = (List<Book>) this.bookRepository.findAll();
+        System.out.println("this is test case for spring boot");
         return list;
+
     }
 
     public Book getBookById(int id) {
+
         Book book = null;
-        book = list.stream().filter(e -> e.getId() == id).findFirst().get();
+        try {
+            // book = list.stream().filter(e -> e.getId() == id).findFirst().get();
+            book = this.bookRepository.findById(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return book;
+
     }
 
     public Book addBook(Book b) {
-        list.add(b);
-        return b;
+        Book result = bookRepository.save(b);
+        return result;
 
     }
+    // 1,2,3,4,5,6,7,8
+    // 5
+    // 7!=5
+    // 1,2,3,4,6,7,8
 
     public void deleteBook(int bid) {
-        list = list.stream().filter(book -> book.getId() != bid).collect(Collectors.toList());
+        bookRepository.deleteById(bid);
+        // list = list.stream().filter(book -> book.getId() !=
+        // bid).collect(Collectors.toList());
 
     }
 
     public void updateBook(Book book, int bookId) {
-        list.stream().map(b -> {
-            if (b.getId() == bookId) {
-                b.setTitle(book.getTitle());
-                b.setAuthor(book.getAuthor());
 
-            }
-            return b;
-        }).collect(Collectors.toList());
+        // list.stream().map(b -> {
+        // if (b.getId() == bookId) {
+        // b.setTitle(book.getTitle());
+        // b.setAuthor(book.getAuthor());
+
+        // }
+        // return b;
+        // }).collect(Collectors.toList());
+
+        book.setId(bookId);
+        bookRepository.save(book);
     }
 
 }
